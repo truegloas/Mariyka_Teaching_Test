e2f98bf0c4592e2f5849c3dc0dc4ff21f84fd49f
truegloas <gloasrow@gmail.com>
2024-07-13 13:40:47 +0700

    Add simple switccher

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let elems = document.getElementsByClassName("heart__circle-param");
-Array.from(elems).forEach(function (elem) {
-    elem.style.backgroundColor = 'red';
-    console.log(elem);
+
+let elemsArray = Array.from(elems);
+
+elemsArray.forEach((elem) => {
+    elem.style.backgroundColor = 'greenyellow';
+})
+
+elemsArray.forEach((elem) => {
+    // if (elem.style.backgroundColor === 'greenyellow') {
+    //     elem.style.backgroundColor = 'red';
+    // } else if (elem.style.backgroundColor === 'red') {
+    //     elem.style.backgroundColor = 'greenyellow';
+    // }
+
+    let es = elem.style;
+    es.backgroundColor = es.backgroundColor === 'greenyellow' ? 'red' : 'greenyellow';
 })
