11442644bf4f4edda752ea3f1aa201d94dbba791
truegloas <gloasrow@gmail.com>
2024-07-13 14:20:31 +0700

    Add color switcher to heart

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ elemsArray.forEach((elem) => {
     elem.style.backgroundColor = 'greenyellow';
 })
 
-elemsArray.forEach((elem) => {
-    // if (elem.style.backgroundColor === 'greenyellow') {
-    //     elem.style.backgroundColor = 'red';
-    // } else if (elem.style.backgroundColor === 'red') {
-    //     elem.style.backgroundColor = 'greenyellow';
-    // }
+document.getElementsByClassName('heart').item(0).onclick = () => {
+    elemsArray.forEach((elem) => {
+        // if (elem.style.backgroundColor === 'greenyellow') {
+        //     elem.style.backgroundColor = 'red';
+        // } else if (elem.style.backgroundColor === 'red') {
+        //     elem.style.backgroundColor = 'greenyellow';
+        // }
 
-    let es = elem.style;
-    es.backgroundColor = es.backgroundColor === 'greenyellow' ? 'red' : 'greenyellow';
-})
+        let es = elem.style;
+        es.backgroundColor = es.backgroundColor === 'greenyellow' ? 'red' : 'greenyellow';
+    })
+}
